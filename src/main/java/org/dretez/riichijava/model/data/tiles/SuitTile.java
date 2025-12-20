0fd4af5b2b6f3246905328771866fa4369a1bff1
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-12-20 11:38:54 +0100

    feat: Validate SuitTile constructor
    - A suit tile's number must be between 1 and 9 (inclusive)

--- src/main/java/org/dretez/riichijava/model/data/tiles/SuitTile.java
+++ src/main/java/org/dretez/riichijava/model/data/tiles/SuitTile.java
@@ -4,7 +4,9 @@ public class SuitTile extends Tile implements Comparable<SuitTile> {
     private final Suit suit;
     private final int number;
 
-    public SuitTile(Suit suit, int number) {
+    public SuitTile(Suit suit, int number) throws IllegalArgumentException{
+        if (number < 1 || number > 9)
+            throw new IllegalArgumentException("Suit tiles range from 1 to 9 (inclusive), received: " + number);
         this.suit = suit;
         this.number = number;
     }
