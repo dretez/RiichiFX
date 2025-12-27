2c655e1cce4e6bf2f71d48fc745a7a6c95a4e524
dretez <105381869+dretez@users.noreply.github.com>
2025-12-27 02:27:07 +0000

    docs: add documentation for `Tile` interface

--- src/main/java/org/dretez/riichijava/model/data/tiles/Tile.java
+++ src/main/java/org/dretez/riichijava/model/data/tiles/Tile.java
@@ -1,7 +1,23 @@
 package org.dretez.riichijava.model.data.tiles;
 
+/**
+ * Represents a Mahjong tile.
+ */
 public sealed interface Tile permits HonourTile, SuitTile {
+    /**
+     * Interprets the Dora using this {@code Tile} as the Dora indicator.
+     *
+     * @return Dora {@code Tile}
+     */
     Tile getDora();
+
+    /**
+     * @return Previous {@code Tile} in the sequence
+     */
     Tile prev();
+
+    /**
+     * @return Next {@code Tile} in the sequence
+     */
     Tile next();
 }
