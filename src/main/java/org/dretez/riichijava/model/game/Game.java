570bd00c383510096804d917fb1cf7994cce0083
dretez <105381869+dretez@users.noreply.github.com>
2025-12-31 01:50:34 +0000

    feat: Update `Game` class

--- src/main/java/org/dretez/riichijava/model/game/Game.java
+++ src/main/java/org/dretez/riichijava/model/game/Game.java
@@ -1,22 +1,39 @@
 package org.dretez.riichijava.model.game;
 
-import org.dretez.riichijava.model.data.wall.Wall;
+import org.dretez.riichijava.model.data.Board;
+import org.dretez.riichijava.model.data.seat.Seat;
 import org.dretez.riichijava.model.data.tiles.enums.Wind;
 
+import java.util.*;
+
 public class Game {
-    private final Wall tiles;
-    private Wind prevalentWind;
-    private Wind dealerWind;
+    private final Board board;
+    private List<Player> players;
+    private List<Seat> seats;
 
     public Game() {
-        tiles = new Wall();
-        prevalentWind = Wind.EAST;
-        dealerWind = Wind.EAST;
+        board = new Board();
+        players = new ArrayList<>(4);
+        seats = new ArrayList<>(4);
+        for (Wind wind : Wind.values())
+            seats.add(new Seat(wind));
     }
 
-    public void nextRound() {
-        dealerWind = dealerWind.next();
-        if (dealerWind == Wind.EAST)
-            prevalentWind = prevalentWind.next();
+    public void start() {
+        Collections.shuffle(players);
+        rotatePlayers();
+    }
+
+    public void draw(Wind seat) throws IllegalArgumentException {
+        if (seat != board.getCurrentSeat())
+            throw new IllegalArgumentException("The " + seat + " seat cannot draw now.");
+        seats.get(seat.ordinal()).player().draw();
+    }
+
+    public void rotatePlayers() {
+        players.addFirst(players.removeLast());
+        for (int i = 0; i < players.size(); i++) {
+            seats.get(i).setPlayer(players.get(i));
+        }
     }
 }
