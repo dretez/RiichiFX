036a13cdaf6b142d9f3b9b06caa018cafdc2c8b2
dretez <105381869+dretez@users.noreply.github.com>
2025-12-31 01:49:03 +0000

    feat: Update `Hand` class

--- src/main/java/org/dretez/riichijava/model/data/Hand.java
+++ src/main/java/org/dretez/riichijava/model/data/Hand.java
@@ -7,9 +7,17 @@ import java.util.ArrayList;
 
 public class Hand {
     private final ArrayList<Tile> tiles;
+    private boolean open;
+
+    private static final TileComparator sorter = new TileComparator();
 
     public Hand() {
         this.tiles = new ArrayList<>(14);
+        this.open = false;
+    }
+
+    public boolean isOpen() {
+        return open;
     }
 
     public void draw(Tile tile) {
@@ -20,7 +28,36 @@ public class Hand {
         return tiles.remove(idx);
     }
 
+    public void pon(Tile tile) {
+        // TODO
+        open = true;
+    }
+
+    public void chi(Tile tile) {
+        // TODO
+        open = true;
+    }
+
+    public void ankan(Tile tile) {
+        // TODO
+        open = true;
+    }
+
+    public void daiminkan(Tile tile) {
+        // TODO
+        open = true;
+    }
+
+    public void shouminkan(Tile tile) {
+        // TODO
+        open = true;
+    }
+
+    public boolean discard(Tile tile) {
+        return tiles.remove(tile);
+    }
+
     public void sort() {
-        tiles.sort(new TileComparator());
+        tiles.sort(sorter);
     }
 }
