595fba36d55279bf4edcbd78f560748e119775e7
André Maltez Nunes <andremalteznunes@sapo.pt>
2026-01-06 16:21:21 +0100

    feat: add `IllegalTileCallException`

---
+++ src/main/java/org/dretez/riichijava/model/exceptions/IllegalTileCallException.java
@@ -0,0 +1,7 @@
+package org.dretez.riichijava.model.exceptions;
+
+public class IllegalTileCallException extends Exception {
+    public IllegalTileCallException(String message) {
+        super(message);
+    }
+}
