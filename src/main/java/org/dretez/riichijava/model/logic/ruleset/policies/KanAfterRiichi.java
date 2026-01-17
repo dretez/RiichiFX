b248e88dcf9e02527c7607133051e1b3f7be51cd
André Maltez Nunes <andremalteznunes@sapo.pt>
2026-01-17 21:21:31 +0100

    feat: reimplement KanAfterRiichi as an ActionPolicy

--- src/main/java/org/dretez/riichijava/model/logic/ruleset/policies/KanAfterRiichi.java
+++ src/main/java/org/dretez/riichijava/model/logic/ruleset/policies/KanAfterRiichi.java
@@ -1,7 +1,18 @@
-package org.dretez.riichijava.model.logic.ruleset.rules;
+package org.dretez.riichijava.model.logic.ruleset.policies;
 
-public enum KanAfterRiichi {
+import org.dretez.riichijava.model.logic.game.action.GameAction;
+
+public enum KanAfterRiichi implements ActionPolicy {
     DISALLOW,
     MUST_KEEP_STRUCTURE,
     MUST_KEEP_WAIT;
+
+    @Override
+    public boolean allows(GameAction action, GameContext ctx) {
+        return action != GameAction.KAN || !ctx.riichiDeclared() || switch (this) {
+            case DISALLOW -> false;
+            case MUST_KEEP_STRUCTURE -> false; // TODO
+            case MUST_KEEP_WAIT -> false; // TODO
+        };
+    }
 }
